f1cc4a7744d0dfa53b6bacc17ba0bfe818e99c15
Dalmazox <kelvi.dalmazo@gmail.com>
2025-05-17 13:23:47 -0300

    chore: under construct

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './global.css'
 function App() {
   return (
     <div className='flex justify-center items-center min-h-screen'>
-      <h1>dalm.dev</h1>
+      <h1>dalm.dev <span className='text-gray-300'>under construct</span></h1>
     </div>
   )
 }
